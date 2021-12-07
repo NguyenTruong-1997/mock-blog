14f8a85a641d98396a26c7353db4adfe88e98efd
NgaDC <ngavip1998@gmail.com>
2021-12-07 09:28:45 +0700

    update Layout

--- src/app/modules/profile/profile.module.ts
+++ src/app/modules/profile/profile.module.ts
@@ -7,25 +7,30 @@ import { ProfileComponent } from './profile.component';
 import { ProfileFavoritesComponent } from './components/profile-favorites/profile-favorites.component';
 import { ProfileArticleComponent } from './components/profile-article/profile-article.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
-
-
+import { MatTabsModule } from '@angular/material/tabs';
 @NgModule({
   declarations: [
     ProfileComponent,
     ProfileArticleComponent,
-    ProfileFavoritesComponent
+    ProfileFavoritesComponent,
   ],
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
+    MatTabsModule,
     RouterModule.forChild([
-      { path: '', component: ProfileArticleComponent },
-      { path: ':username', component: ProfileArticleComponent },
-      { path: ':username/favorites', component: ProfileFavoritesComponent }
-    ])
+      {
+        path: '',
+        component: ProfileComponent,
+        children: [
+          { path: '', redirectTo: 'username', pathMatch: 'full' },
+          { path: ':username', component: ProfileArticleComponent },
+          { path: ':username/favorites', component: ProfileFavoritesComponent },
+        ],
+      },
+    ]),
   ],
-  providers: [ProfileService]
+  providers: [ProfileService],
 })
-export class ProfileModule { }
+export class ProfileModule {}
