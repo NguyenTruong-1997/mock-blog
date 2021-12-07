d8ea1e9bf953ea0c8fef55806e887f6f71d08af0
trungsyk58utc <cayconlontonzz@gmail.com>
2021-12-07 14:30:36 +0700

    update-layout

--- src/app/modules/article/article.component.ts
+++ src/app/modules/article/article.component.ts
@@ -3,20 +3,28 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
-  styleUrls: ['./article.component.scss']
+  styleUrls: ['./article.component.scss'],
 })
 export class ArticleComponent implements OnInit {
   //#region Properties
-
+  follow: boolean = false;
+  favorites: boolean = false;
   //#end region
 
   //#region Constructor
-  public constructor() { }
+  public constructor() {}
 
   //#end region
 
   //#region Methods
-  public ngOnInit(): void { }
+  public ngOnInit(): void {}
 
   //#end region
+  changeFollow() {
+    this.follow = !this.follow;
+  }
+
+  changeFavorite() {
+    this.favorites = !this.favorites;
+  }
 }
--- src/app/modules/article/article.module.ts
+++ src/app/modules/article/article.module.ts
@@ -2,18 +2,14 @@ import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ArticleComponent } from './article.component';
-
-
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-  declarations: [
-    ArticleComponent
-  ],
+  declarations: [ArticleComponent],
   imports: [
     CommonModule,
-    RouterModule.forChild([
-      { path: '', component: ArticleComponent }
-    ])
-  ]
+    RouterModule.forChild([{ path: '', component: ArticleComponent }]),
+    MatInputModule,
+  ],
 })
-export class ArticleModule { }
+export class ArticleModule {}
